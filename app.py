6fda6fc289e0e388d50d2d47ca87f9bd97d8e700
Jimmy <miki3867@gmail.com>
2021-05-17 13:08:06 +0200

    Add requirements.txt

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+# Here insert your db in my case it's my local postgres
+# And remember never add passwords to public repos :p
 app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://testuser:dupa1234@localhost/test"
 db = SQLAlchemy(app)
+
 from models import *
 
 print("Creating database...")
